9231e0e1dc4b1c2854603f5c502e8e061019f026
julianmendozamt <julian.mendoza.mt@correo.usa.edu.co>
2021-10-16 10:39:33 -0500

    clase viernes 15

---
+++ src/main/java/grupo32_13_10/proyecto32_13_10/Proyecto321310Application.java
@@ -0,0 +1,13 @@
+package grupo32_13_10.proyecto32_13_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto321310Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto321310Application.class, args);
+	}
+
+}
---
+++ src/test/java/grupo32_13_10/proyecto32_13_10/Proyecto321310ApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo32_13_10.proyecto32_13_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto321310ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
