8556176f4b409ecac11476ed8668f01a702f7427
julianmendozamt <julian.mendoza.mt@correo.usa.edu.co>
2021-10-17 06:05:06 -0500

    actualizacion 17/10/21

--- target/classes/static/funciones.js
+++ target/classes/static/funciones.js
@@ -1,67 +1,74 @@
 /**
  * funciones para bicicleta
  */
-function guardarDatos(){
-
-    var datos={
-        id:$("#miId").val(),
-        brand:$("#brand").val(),
-        model:$("#model").val(),
-        category_id:$("#category_id").val(),
-        name:$("#name").val()
-        }
-
-    var dataToSend=JSON.stringify(datos);
-    $.ajax({
-
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike" ,
-        type: 'POST',
-        data:datos,
-        dataType: 'JSON',      
-        
-        success:function(response) {
-            console.log(response);
-            alert("Guardado Exitoso")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
-    });
-}
-
 function ConsultarDatos(){
     $.ajax({
 
         dataType: 'JSON',
         url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike" ,
+        
         type: 'GET',
-        contentType: 'application/json',
-        success:function(response) {
+        success: function(response){
+            console.log(response);
+            $("#resultadoBike").empty();
+            respuestaBike(resultadoBike.items);
+        }
+    });
+}
+function respuestaBike(items){
+    
+            let myTable="<table border=1>";
 
-            var items=response.items;
-
-            for(i=0;i<items.length;i++){
-                $("#items").append("<td>");
-                $("#items").append("<td>"+items[i].id+"</td>");
-                $("#items").append("<td>"+items[i].brand+"</td>");
-                $("#items").append("<td>"+items[i].model+"</td>");
-                $("#items").append("<td>"+items[i].category_id+"</td>");
-                $("#items").append("<td>"+items[i].name+"</td>");
-                $("#items").append('<td><button onclick="borrarDatos('+items[i].id+')">Borrar</button></td>');
-                $("#items").append('<td><button onclick="obtenerIdEspecifico('+items[i].id+')">Cargar</button></td>');
-                $("#items").append("</td>");
+            for(i=0; i<items.length; i++){
+                myTable += "<tr>";
+                myTable += "<td>"+items[i].id+"</td>";
+                myTable += "<td>"+items[i].brand+"</td>";
+                myTable += "<td>"+items[i].model+"</td>";
+                myTable += "<td>"+items[i].category_id+"</td>";
+                myTable += "<td>"+items[i].name+"</td>";
+                myTable += "<td><button onclick='borrarDatos("+items[i].id+")'>Borrar</button></td>";
+                myTable += "<td><button onclick='obtenerIdEspecifico("+items[i].id+")'>Cargar</button></td>";
+                myTable += "</tr>";
             }
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-                }
-            });
-}  
+            myTable += "</table>";
+            $("#resultadoBike").append(myTable);
+}
+function guardarDatos(){
 
-function borrarDatos(items){
-    var datos={
+    let misDatos={
+        id:$("#miId").val(),
+        brand:$("#brand").val(),
+        model:$("#model").val(),
+        category_id:$("#category_id").val(),
+        name:$("#name").val()
+    };
+
+    let dataToSend=JSON.stringify(misDatos);
+    $.ajax({
+
+        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike" ,
+        type: 'POST',
+        data:misDatos,
+        dataType: 'JSON',      
+        
+        success:function(response) {
+            $("#resultadoBike").empty();
+            $("#miId").val("");
+            $("#brand").val("");
+            $("#model").val("");
+            $("#category_id").val("");
+            $("#name").val("");
+            ConsultarDatos();
+            alert("Datos Guardados Con Exito")
+        }
+    });
+}
+function borrarDatos(idItems){
+    let misDatos={
         id:$("#miId").val(),
     }
 
-    var dataToSend=JSON.stringify(datos);
+    let dataToSend=JSON.stringify(misDatos);
     $.ajax({
 
         dataType: 'JSON',
@@ -70,24 +77,22 @@ function borrarDatos(items){
         type: 'DELETE',
         contentType: 'application/json',
         success:function(response) {
-            console.log(response);
+            ConsultarDatos();
+            alert("Datos Borrados Con Exito")
         },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
+        
     });
 }
-function obtenerIdEspecifico(idItem){
-
+function obtenerIdEspecifico(items){
+    console.log("---- obtenerIdEspecifico items", items);
     $.ajax({
 
         dataType: 'JSON',
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike"+idItem ,
+        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike"+idItems ,
         type: 'GET',
-        contentType: 'application/json',
         success:function(response) {
-            console.log(response);
+            console.log("---- obtenerIdEspecifico", response);
             var item=response.items[0];
-
             $("#miId").val(item.id),
             $("#brand").val(item.brand),
             $("#model").val(item.model),
@@ -102,7 +107,7 @@ function obtenerIdEspecifico(idItem){
 
 function actualizarDatos(){
 
-    var datos={
+    let misDatos={
         id:$("#miId").val(),
         brand:$("#brand").val(),
         model:$("#model").val(),
@@ -110,176 +115,32 @@ function actualizarDatos(){
         name:$("#name").val()
         }
 
-    var dataToSend=JSON.stringify(datos);
+    let dataToSend=JSON.stringify(misDatos);
     $.ajax({
 
         dataType: 'JSON',
         data:dataToSend,
         url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike" ,
-        type: 'POST',
+        type: 'PUT',
         contentType: 'application/json',
         success:function(response) {
-            console.log(response);
+            $("#resultadoBike").empty();
+            $("#miId").val("");
+            $("#brand").val("");
+            $("#model").val("");
+            $("#category_id").val("");
+            $("#name").val("");
+            ConsultarDatos();
+            alert("Datos Actualizados Con Exito")
+            
         },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
     });
-}boton
+}
+
 /**
  * funciones para cliente
  */
-function guardarCliente(){
 
-    var datos={
-        id:$("#miId").val(),
-        name:$("#name").val(),
-        email:$("#email").val(),
-        age:$("#age").val()
-        }
-
-    var dataToSend=JSON.stringify(datos);
-    $.ajax({
-
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client" ,
-        type: 'POST',
-        data:datos,
-        dataType: 'JSON',      
-        
-        success:function(response) {
-            console.log(response);
-            alert("Guardado Exitoso")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
-    });
-}
-function ConsultarCliente(){
-    $.ajax({
-
-        dataType: 'JSON',
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/client/client" ,
-        type: 'GET',
-        contentType: 'application/json',
-        success:function(response) {
-
-            var items=response.items;
-
-            for(i=0;i<items.length;i++){
-                $("#items").append("<td>");
-                $("#items").append("<td>"+items[i].id+"</td>");
-                $("#items").append("<td>"+items[i].name+"</td>");
-                $("#items").append("<td>"+items[i].email+"</td>");
-                $("#items").append("<td>"+items[i].age+"</td>");
-                $("#items").append('<td><button onclick="borrarDatos('+items[i].id+')">Borrar</button></td>');
-                $("#items").append('<td><button onclick="obtenerIdEspecifico('+items[i].id+')">Cargar</button></td>');
-                $("#items").append("</td>");
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-                }
-            });
-}  
-function actualizarCliente(){
-
-    var datos={
-        id:$("#miId").val(),
-        brand:$("#brand").val(),
-        model:$("#model").val(),
-        category_id:$("#category_id").val(),
-        name:$("#name").val()
-        }
-
-    var dataToSend=JSON.stringify(datos);
-    $.ajax({
-
-        dataType: 'JSON',
-        data:dataToSend,
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/bike/bike" ,
-        type: 'POST',
-        contentType: 'application/json',
-        success:function(response) {
-            console.log(response);
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
-    });
-}
 /**
  * funciones para mensaje
  */
-function guardarMensaje(){
-
-    var datos={
-        id:$("#miId").val(),
-        name:$("#name").val(),
-        email:$("#email").val(),
-        age:$("#age").val()
-        }
-
-    var dataToSend=JSON.stringify(datos);
-    $.ajax({
-
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message" ,
-        type: 'POST',
-        data:datos,
-        dataType: 'JSON',      
-        
-        success:function(response) {
-            console.log(response);
-            alert("Guardado Exitoso")
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
-    });
-}
-function ConsultarMensaje(){
-    $.ajax({
-
-        dataType: 'JSON',
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message" ,
-        type: 'GET',
-        contentType: 'application/json',
-        success:function(response) {
-
-            var items=response.items;
-
-            for(i=0;i<items.length;i++){
-                $("#items").append("<td>");
-                $("#items").append("<td>"+items[i].id+"</td>");
-                $("#items").append("<td>"+items[i].name+"</td>");
-                $("#items").append("<td>"+items[i].email+"</td>");
-                $("#items").append("<td>"+items[i].age+"</td>");
-                $("#items").append('<td><button onclick="borrarDatos('+items[i].id+')">Borrar</button></td>');
-                $("#items").append('<td><button onclick="obtenerIdEspecifico('+items[i].id+')">Cargar</button></td>');
-                $("#items").append("</td>");
-            }
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-                }
-            });
-}  
-function actualizarMensaje(){
-
-    var datos={
-        id:$("#miId").val(),
-        brand:$("#brand").val(),
-        model:$("#model").val(),
-        category_id:$("#category_id").val(),
-        name:$("#name").val()
-        }
-
-    var dataToSend=JSON.stringify(datos);
-    $.ajax({
-
-        dataType: 'JSON',
-        data:dataToSend,
-        url: "https://g9b9523a5debfd4-dbreto1.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/message/message" ,
-        type: 'POST',
-        contentType: 'application/json',
-        success:function(response) {
-            console.log(response);
-        },
-        error: function(jqXHR, textStatus, errorThrown){
-        }
-    });
-}
